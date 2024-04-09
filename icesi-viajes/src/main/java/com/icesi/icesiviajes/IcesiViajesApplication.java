d3c2b93f7d8a2f1956c018885e9ac9853b794c54
John Kennedy Landazuri Sandoval <jkls1998@gmail.com>
2024-04-09 08:56:24 -0500

    project added

---
+++ icesi-viajes/src/main/java/com/icesi/icesiviajes/IcesiViajesApplication.java
@@ -0,0 +1,13 @@
+package com.icesi.icesiviajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcesiViajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcesiViajesApplication.class, args);
+	}
+
+}
---
+++ icesi-viajes/src/test/java/com/icesi/icesiviajes/IcesiViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icesi.icesiviajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcesiViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
